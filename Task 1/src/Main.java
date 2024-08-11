7b06e7b54ea8a1dc3d8322818b16afbdaaea98df
BorisRojnov <BorisRojnov@yandex.by>
2024-08-11 12:44:19 +0300

    homework complete

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     static double a = 0;
     static double b = 0;
@@ -15,22 +17,31 @@ public class Main {
 
     private static void divide() {
         //todo дописать логику и печать результата
+        double c = a / b;
+        System.out.println("Частное "+ a + " и " + b + " = " + c);
     }
 
     private static void diff() {
         //todo дописать логику и печать результата
+        double c = a - b;
+        System.out.println("Вычитание "+ a + " и " + b + " = " + c);
     }
 
     private static void multiply() {
         //todo дописать логику и печать результата
+        double c = a * b;
+        System.out.println("Произведение "+ a + " и " + b + " = " + c);
     }
 
     private static void sum() {
         //todo дописать логику и печать результата
+        double c = a + b;
+        System.out.println("Сумма "+ a + " и " + b + " = " + c);
     }
 
     private static double inputDouble() {
         //todo дописать логику
-        return 0;
+        System.out.println("Введите число: ");
+        return new Scanner(System.in).nextDouble();
     }
 }
